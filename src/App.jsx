25a72b403f6ef5e625aee6d285647e268c6e881b
unknown <Shahnawazunar>
2023-03-10 21:32:41 -0800

    React First Assignment

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Aap from './App.css'
+
+const App = () => {
+    return (
+        <>
+               <h1>Shah Nawaz Unar</h1>
+               <div class="main">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+    </div>
+<h2>First Assignment Of React JS</h2>
+ 
+        </>
+    )
+}
+
+export default App;
